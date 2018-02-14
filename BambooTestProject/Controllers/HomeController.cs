942edbdc1f26e501648564a442b590814d72bbc5
ITRANSITION\a.tarasenko <a.tarasenko@itransition.com>
2018-02-14 19:34:28 +0300

    Pull request

--- BambooTestProject/Controllers/HomeController.cs
+++ BambooTestProject/Controllers/HomeController.cs
@@ -1,15 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft;
+using Newtonsoft.Json;
 
 namespace BambooTestProject.Controllers
 {
     public class HomeController : Controller
     {
+        private string GetPlanName()
+        {
+            var Url = "http://localhost:8085/rest/api/latest/result.json?os_authType=basic";
+            var webRequest = System.Net.WebRequest.Create(Url);
+            if (webRequest != null)
+            {
+                webRequest.Method = "GET";
+                webRequest.Timeout = 12000;
+                webRequest.ContentType = "application/json";
+                string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("a.tarasenko:123123a"));
+                webRequest.Headers.Add("Authorization", "Basic " + svcCredentials);
+                using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+                {
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+                    {
+                        var jsonResponse = sr.ReadToEnd();
+                        dynamic json = JsonConvert.DeserializeObject(jsonResponse);
+                        return json.results.result.plan.key;
+                    }
+                }
+            }
+            return "DEVELOPER";
+
+        }
+
         public ActionResult Index()
         {
+            var Url = "http://localhost:8085/rest/api/latest/result/BAM-PN/branch/development.json?expand=results[-10:-1].result?os_authType=basic";
+            try
+            {
+                var webRequest = System.Net.WebRequest.Create(Url);
+                if (webRequest != null)
+                {
+                    webRequest.Method = "GET";
+                    webRequest.Timeout = 12000;
+                    webRequest.ContentType = "application/json";
+                    string svcCredentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes("a.tarasenko:123123a"));
+                    webRequest.Headers.Add("Authorization", "Basic " + svcCredentials);
+                    using (System.IO.Stream s = webRequest.GetResponse().GetResponseStream())
+                    {
+                        using (System.IO.StreamReader sr = new System.IO.StreamReader(s))
+                        {
+                            var jsonResponse = sr.ReadToEnd();
+                            Console.WriteLine(String.Format("Response: {0}", jsonResponse));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
             return View();
         }
 
